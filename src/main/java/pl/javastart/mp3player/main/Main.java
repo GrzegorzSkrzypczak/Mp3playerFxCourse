5121c1d8cb78dd50cb54b9e91a9b8e3eaeb06364
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-06-03 08:19:00 +0200

    stworzone okno pod aplikacje mp3

--- src/main/java/pl/javastart/mp3player/main/Main.java
+++ src/main/java/pl/javastart/mp3player/main/Main.java
@@ -3,7 +3,7 @@ package pl.javastart.mp3player.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Pane mainPane = FXMLLoader.load(getClass().getResource("/mainPane.fxml"));
+        BorderPane mainPane = FXMLLoader.load(getClass().getResource("/mainPane.fxml"));
         Scene scene = new Scene(mainPane);
         stage.setScene(scene);
         stage.setTitle("Mp3 Player");
