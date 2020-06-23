e03d6a457d0c973b3bfc6ae4b7f3e270bfa48578
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-06-23 08:19:07 +0200

    Potworzone odpowiednie klasy oraz fxml'e kontrolujące okna

--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,20 +1,9 @@
 package pl.javastart.mp3player.controller;
 
-import javafx.scene.control.*;
 
 public class MainController {
-    public MenuItem fileMenuItem;
-    public MenuItem dirMenuItem;
-    public MenuItem closeMenuItem;
-    public MenuItem aboutMenuItem;
-    public TableView<?> contentTable;
-    public Button previousButton;
-    public ToggleButton playButton;
-    public Button nextButton;
-    public Slider volumeSlider;
-    public Slider progressSlider;
 
-    public void initialize () {
-        System.out.println("MainController created");
+    public void initialize() {
+        System.out.println("Main controller created");
     }
 }
